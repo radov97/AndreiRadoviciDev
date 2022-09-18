363a7218c513093a995fb863cd7afb685b0f6fcc
AndreiRadovici <andreirdv97@gmail.com>
2022-09-18 13:42:22 +0100

    4rd commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, this is going to be my personal website 3rd commit</p>
+        <p>Hello, this is going to be my personal website 4rd commit</p>
         <a
           className="App-link"
           href="https://reactjs.org"
