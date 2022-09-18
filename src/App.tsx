aceedd7b70805cb145a7e873fce4f8fc07628e8c
AndreiRadovici <andreirdv97@gmail.com>
2022-09-18 13:55:42 +0100

    Clean folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <p>Hello, this is going to be my personal website 4rd commit</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
