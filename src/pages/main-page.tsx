679e45b1bd4df71358fa0577477b5ecb1b8e739c
AndreiRadovici <andreirdv97@gmail.com>
2022-09-19 23:51:09 +0100

    Save progress

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,12 +1,19 @@
 import React, { FC, useContext } from "react";
 import { ResponsiveContext } from "../general/context";
+import { Container } from "../styles/main-page-style";
 
 const MainPage: FC = (): JSX.Element => {
   const { isBigScreen, isTabletOrMobile } = useContext(ResponsiveContext);
 
   console.log("isBigScreen", isBigScreen, "isTabletOrMobile", isTabletOrMobile);
 
-  return <div>Hello World, this is my first styled component!</div>;
+  return (
+    <Container>
+      <div>Header</div>
+      <div>Content</div>
+      <div>Footer</div>
+    </Container>
+  );
 };
 
 export default MainPage;
