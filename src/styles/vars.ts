7409e2905ff88b57317288182922b7869417611a
AndreiRadovici <andreirdv97@gmail.com>
2022-09-19 22:32:47 +0100

    Save colours

--- src/styles/vars.ts
+++ src/styles/vars.ts
@@ -1,2 +1,10 @@
+// Break points
 export const tabletBreakpoint = "1224px";
 export const desktopBreakpoint = "1225px";
+
+// Colours
+export const primaryRed = "#e63946";
+export const lightMint = "#e63946";
+export const lightBlue = "#a8dadc";
+export const primaryBlue = "#457b9d";
+export const darkBlue = "#1d3557";
