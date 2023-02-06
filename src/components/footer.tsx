f9ad42ef515186590d4be44a00940c72b0b0abfa
Andrei Radovici <andrei.radovici@miralis.co.uk>
2023-02-06 21:18:32 +0000

    Replace styled objects with css modules

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,14 @@
 import { FC } from "react";
 import { SocialIcon } from "react-social-icons";
-import {
-  CenterContainer,
-  Container,
-  Copyright,
-  SocialMediaWrapper,
-} from "../styles/footer-style";
 import { White } from "../styles/vars";
 import { CgCopyright } from "react-icons/cg";
+import styles from "./../styles/footer.module.css";
 
 const Footer: FC = (): JSX.Element => {
   return (
-    <Container>
-      <CenterContainer>
-        <SocialMediaWrapper>
+    <div className={styles.container}>
+      <div className={styles.centerContainer}>
+        <div className={styles.socialMediaWrapper}>
           <SocialIcon
             url="https://www.facebook.com/andrei.rdv.5"
             fgColor={White}
@@ -31,12 +26,12 @@ const Footer: FC = (): JSX.Element => {
             fgColor={White}
             url="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSMVVxPgFhGwTrLVsMdqdKjsVJjCZhSjVcrrktSTNxvdQkXDGrpvFfrHHTjFLTGclRvkJjNx"
           />
-        </SocialMediaWrapper>
-        <Copyright>
+        </div>
+        <div className={styles.copyright}>
           <CgCopyright /> {new Date().getFullYear()} Copyright: Andrei Radovici
-        </Copyright>
-      </CenterContainer>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 };
 export default Footer;
--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,27 +1,21 @@
 import { FC } from "react";
 import { IChildrenProp } from "../general/interfaces";
-import {
-  Container,
-  NavbarDiv,
-  ChildrenDiv,
-  FooterDiv,
-  BodyDiv,
-} from "../styles/page-style";
 import Footer from "./footer";
 import Navbar from "./navbar";
+import styles from "./../styles/page.module.css";
 
 const Page: FC<IChildrenProp> = ({ children }): JSX.Element => (
-  <Container>
-    <NavbarDiv>
+  <div className={styles.container}>
+    <div className={styles.navbar}>
       <Navbar />
-    </NavbarDiv>
-    <BodyDiv>
-      <ChildrenDiv>{children}</ChildrenDiv>
-      <FooterDiv>
+    </div>
+    <div className={styles.body}>
+      <div className={styles.children}>{children}</div>
+      <div className={styles.footer}>
         <Footer />
-      </FooterDiv>
-    </BodyDiv>
-  </Container>
+      </div>
+    </div>
+  </div>
 );
 
 export default Page;
